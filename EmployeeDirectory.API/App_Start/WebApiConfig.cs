306b521120e77f804f81939b9f56c19c27d395d7
emorales <emorales0024@gmail.com>
2014-10-07 18:10:54 -0400

    ~Started web api project ~Added Owin Startup class ~Added serialization default settings for camel case ~Added nuget package for ASP.NET Identity Owin ~Added package for Entity Framework to be used with the APS.NET Identity. ~Added AuthContext ~Added UserModel ~Added MSSQL connection string in web.config

---
+++ EmployeeDirectory.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+
+namespace EmployeeDirectory.API
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // map web api routes
+            config.MapHttpAttributeRoutes();
+
+            // set default http route
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // set json serialization settings eg. camel case
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ EmployeeDirectory.API/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(EmployeeDirectory.API.Startup))]
+namespace EmployeeDirectory.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+
+    }
+}
